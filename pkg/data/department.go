ac8d4c1632c0dcbe98467a000ca697686019d8ea
Ton <lyyyyych@gmail.com>
2021-04-28 14:55:09 +0300

    fix remarks after review

--- pkg/data/department.go
+++ pkg/data/department.go
@@ -19,42 +19,42 @@ func NewDepartmentData(db *gorm.DB) *DepartmentData {
 	return &DepartmentData{db: db}
 }
 
-func (u DepartmentData) Add(department Department) (int, error) {
-	result := u.db.Create(&department)
+func (d DepartmentData) Add(department Department) (int, error) {
+	result := d.db.Create(&department)
 	if result.Error != nil {
 		return -1, fmt.Errorf("can't create department, error: %w", result.Error)
 	}
 	return department.Code, nil
 }
 
-func (u DepartmentData) Read(code int) (Department, error) {
+func (d DepartmentData) Read(code int) (Department, error) {
 	var department Department
-	result := u.db.Find(&department, code)
+	result := d.db.Find(&department, code)
 	if result.Error != nil {
 		return department, fmt.Errorf("can't read department with given id, error: %w", result.Error)
 	}
 	return department, nil
 }
 
-func (u DepartmentData) ReadAll() ([]Department, error) {
+func (d DepartmentData) ReadAll() ([]Department, error) {
 	var departments []Department
-	result := u.db.Find(&departments)
+	result := d.db.Find(&departments)
 	if result.Error != nil {
 		return nil, fmt.Errorf("can't read departments from database, error: %w", result.Error)
 	}
 	return departments, nil
 }
 
-func (u DepartmentData) ChangeName(code int, name string) (int, error) {
-	result := u.db.Model(Department{}).Where("code = ", code).Update("name", name)
+func (d DepartmentData) ChangeName(code int, name string) (int, error) {
+	result := d.db.Model(Department{}).Where("code = ", code).Update("name", name)
 	if result.Error != nil {
 		return -1, fmt.Errorf("can't update name, error: %w", result.Error)
 	}
 	return code, nil
 }
 
-func (u DepartmentData) Delete(code int) error {
-	result := u.db.Delete(&Department{}, code)
+func (d DepartmentData) Delete(code int) error {
+	result := d.db.Delete(&Department{}, code)
 	if result.Error != nil {
 		return fmt.Errorf("can't delete department from database, error: %w", result.Error)
 	}
--- pkg/data/student.go
+++ pkg/data/student.go
@@ -21,51 +21,51 @@ func NewStudentData(db *gorm.DB) *StudentData {
 	return &StudentData{db: db}
 }
 
-func (u StudentData) Add(student Student) (int, error) {
-	result := u.db.Create(&student)
+func (s StudentData) Add(student Student) (int, error) {
+	result := s.db.Create(&student)
 	if result.Error != nil {
 		return -1, fmt.Errorf("can't create student, error: %w", result.Error)
 	}
 	return student.Id, nil
 }
 
-func (u StudentData) Read(id int) (Student, error) {
+func (s StudentData) Read(id int) (Student, error) {
 	var student Student
-	result := u.db.Find(&student, id)
+	result := s.db.Find(&student, id)
 	if result.Error != nil {
 		return student, fmt.Errorf("can't read student with given id, error: %w", result.Error)
 	}
 	return student, nil
 }
 
-func (u StudentData) ReadAll() ([]Student, error) {
+func (s StudentData) ReadAll() ([]Student, error) {
 	var students []Student
-	result := u.db.Find(&students)
+	result := s.db.Find(&students)
 	if result.Error != nil {
 		return nil, fmt.Errorf("can't read students from database, error: %w", result.Error)
 	}
 	return students, nil
 }
 
-func (u StudentData) ChangeStatus(id int) (int, error) {
-	result := u.db.Exec("UPDATE students SET is_active = NOT is_active WHERE id = ?", id)
+func (s StudentData) ChangeStatus(id int) (int, error) {
+	result := s.db.Exec("UPDATE students SET is_active = NOT is_active WHERE id = ?", id)
 	if result.Error != nil {
 		return -1, fmt.Errorf("can't update student status, error: %w", result.Error)
 	}
 	return id, nil
 }
 
-func (u StudentData) Delete(id int) error {
-	result := u.db.Delete(&Student{}, id)
+func (s StudentData) Delete(id int) error {
+	result := s.db.Delete(&Student{}, id)
 	if result.Error != nil {
 		return fmt.Errorf("can't delete student from database, error: %w", result.Error)
 	}
 	return nil
 }
 
-func (u StudentData) GetCurrentRate(id int) (float64, error) {
+func (s StudentData) GetCurrentRate(id int) (float64, error) {
 	var avg float64
-	result := u.db.Model(&Student{}).
+	result := s.db.Model(&Student{}).
 		Select("AVG(enrollments.average_grade)").
 		Joins("join enrollments on id = enrollments.student_id").
 		Where("students.id = ?", id).
@@ -76,9 +76,9 @@ func (u StudentData) GetCurrentRate(id int) (float64, error) {
 	return avg, nil
 }
 
-func (u StudentData) GetCoursesList(id int) ([]Course, error) {
+func (s StudentData) GetCoursesList(id int) ([]Course, error) {
 	var courses []Course
-	result := u.db.Model(&Student{}).
+	result := s.db.Model(&Student{}).
 		Select("courses.code, courses.title, courses.department_code, courses.description").
 		Joins("join enrollments on id = enrollments.student_id").
 		Joins("join lessons on enrollments.lesson_id = lessons.id").
